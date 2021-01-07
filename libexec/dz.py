a0fa8a49e8decf1a6bec0a08aaf6127224f112d0
Awesomeslyerg <rarfbd@gmail.com>
2021-01-07 14:38:54 -0800

    Update dz.py
    Added Uknown9 for proper parsing

--- libexec/dz.py
+++ libexec/dz.py
@@ -159,8 +159,9 @@ class DZFile(DZStruct):
 		('formatMinor',	('I',    False)),	# always 1 in LE
 		('reserved0',	('I',    True)),	# format patchlevel?
 		('device',	('32s',  True)),
-		('version',	('144s', True)),	# "factoryversion"
-		('chunkCount',	('I',    False)),
+		('version',	('121s', True)),	# "factoryversion"
+		('unknown9',    ('23s',  False)),       # "md5?"
+'		('chunkCount',	('I',    False)),
 		('md5',		('16s',  False)),	# MD5 of chunk headers
 		('unknown0',	('I',    False)),	# 256?
 		('reserved1',	('I',    True)),	# currently always zero
